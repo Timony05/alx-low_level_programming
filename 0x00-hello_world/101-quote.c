29e136ee97935c8d7f930746223c499abf54193f
Timony05 <tori12timony@gmail.com>
2023-02-16 17:33:19 -0800

    Task9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,14 @@
 #include <unistd.h>
-
 /**
  * main - Entry point of the program
  *
- * Description: Prints the exact phrase "and that piece of art is useful"
- *              - Dora Korpar, 2015-10-19, followed by a new line, to the
- *              standard error.
  * Return: 1
- */
+*/
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = 59;
-
-    write(2, str, len);
-    return (1);
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = 59;
+	write(2, str, len);
+	return (1);
 }
 
