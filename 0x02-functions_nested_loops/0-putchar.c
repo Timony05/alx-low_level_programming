51b35138347780278fcf2b6826cb40421f289279
Timony05 <tori12timony@gmail.com>
2023-02-21 16:42:16 -0800

    Task 0 to 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - Print _putchar
+ * main - print _putchar
+ * 8s written by Victoria
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
